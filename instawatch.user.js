701559b9823f5b88c8fef98d3230ee60756c4d20
xi xiniping <xxinping@users.noreply.github.com>
2016-11-15 18:31:04 -0800

    ad nam

--- instawatch.user.js
+++ instawatch.user.js
@@ -3,9 +3,9 @@
 // @namespace    https://github.com/xxinping/InstaWatch
 // @include      https://www.instagc.com/watch/*
 // @updateURL    https://raw.githubusercontent.com/xxinping/InstaWatch/master/instawatch.user.js#bypass=true
-// @version      0.1.0
+// @version      0.1.1
 // @description  Keep Watch working
-// @author       0r4ng3l0rd
+// @author       xi xinping, 0r4ng3l0rd
 // @grant        none
 // @require      https://raw.githubusercontent.com/kamranahmedse/jquery-toast-plugin/bd761d335919369ed5a27d1899e306df81de44b8/dist/jquery.toast.min.js
 // ==/UserScript==
@@ -20,8 +20,8 @@ function injectStylesheet(url) {
     //inject jqtoast
     injectStylesheet("https://cdn.rawgit.com/kamranahmedse/jquery-toast-plugin/bd761d335919369ed5a27d1899e306df81de44b8/dist/jquery.toast.min.css");
     $.toast({
-        text: 'The InstaWatcher script is running!<br><a target="_blank" href="https://google.com">Google</a>',
-        heading: 'InstaWatcher', // Optional heading to be shown on the toast
+        text: 'The InstaWatcher script is running!<br>thanks to xi xinping<br><a target="_blank" href="https://google.com">Google</a>',
+        heading: 'InstaWatcher (xx)', // Optional heading to be shown on the toast
         loader: true,
         loaderBg: '#9EC600',
         showHideTransition: 'slide', // fade, slide or plain
@@ -36,12 +36,12 @@ function injectStylesheet(url) {
     //wait a second, then expand everything
     $(document).ready(()=>{
         setTimeout(()=>{
-            console.log("Everything should be loaded, clicking show");
+            console.log("Everything should be loaded, clicking show --xi");
             $(".playlist-show").click();
         }, 1000);
     });
 
-    let refreshTime = 30*60*1000;
+    let refreshTime = 32*60*1000;
     setTimeout(function () {
         location.reload();
     }, refreshTime);
